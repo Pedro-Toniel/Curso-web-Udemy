ec134a3ce8f95e11d3fd0282dc27c0193249755c
Pedro Toniel <patoni1997@gmail.com>
2022-08-25 18:31:33 -0300

    Create comentário.js

---
+++ Seção 3/comentário.js
@@ -0,0 +1,6 @@
+// console.log é usado para exibir algo na tela do computador
+console.log('my name ys Pedro');//comentários poder ser escritos inline tbm!
+/*
+para comentários em multiplas linhas 
+utiliza-se este formato
+*/
