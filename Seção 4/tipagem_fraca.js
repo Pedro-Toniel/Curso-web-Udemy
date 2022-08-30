4a889106f5fd8f5b859166d2a521bb029c86d049
Pedro Toniel <patoni1997@gmail.com>
2022-08-30 15:02:52 -0300

    Create tipagem_fraca.js

---
+++ Seção 4/tipagem_fraca.js
@@ -0,0 +1,15 @@
+let qualquer = 'legal';
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.1516
+console.log(qualquer);
+console.log(typeof qualquer);
+
+//Evitar nomes genéricos e siglas 
+let valor = '';
+let numero = 1;
+let pqp = false //produto quimico perigoso... kkk
+
+/*utilizar nomes de variaveis, mais legiveis possivel.
+evitar o uso exessivo de comentários*/
