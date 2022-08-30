99f54a4775e29207cf0140bd752c39ccac6727a4
Pedro Toniel <patoni1997@gmail.com>
2022-08-30 15:02:11 -0300

    Create comentarios.js

---
+++ Seção 4/comentarios.js
@@ -0,0 +1,9 @@
+// comentário de uma linha 
+console.log('linha 1');
+/* comentários 
+de multiplas 
+linhas */
+console.log('linha 2');
+
+/* Utilizar comentários 
+que sejam realmente necessários */
